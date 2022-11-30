847f5760719900d6a97e7f1da15d8ba4039c5874
observerrei <116433427+observerrei@users.noreply.github.com>
2022-12-01 02:50:55 +1000

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -25,5 +25,5 @@ class CryptoConverter:
         except ValueError:
             raise ConvertionException(f"Не удалось обработать количество {amount}")
         r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}")
-        total_base = json.loads(r.content)[keys[base]]
+        total_base = json.loads(r.content)[keys[base]] * amount
         return total_base
